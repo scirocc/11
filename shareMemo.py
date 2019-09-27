6515aa9c5a1bf315c17aa5f094b8eff27c967a22
scirocc <m19802253786@163.com>
2019-09-27 17:55:56 +0800

    Initial commit

---
+++ shareMemo.py
@@ -0,0 +1,30 @@
+import multiprocessing as mp
+import os
+import time
+
+# def func(x):
+#     pass
+#
+#
+# def test():
+#     # a=list([i for i in range(10**7)])
+#     manager = mp.Manager()
+#     a=manager.list([i for i in range(10**7)])
+#     # print(a)
+#     time1=time.time()
+#     numsOfmp=os.cpu_count()*2
+#     smp=[]
+#     for i in range(numsOfmp):
+#         smp.append(mp.Process(target=func,args=(a,)))
+#     for i in smp:
+#         i.start()
+#         print(i)
+#     for i in smp:i.join()
+#     time2=time.time()
+#     print(time2-time1)
+# if __name__=='__main__':
+#     test()
+
+# file=
+# mp.sharedctypes.
+print('%s','asd')
